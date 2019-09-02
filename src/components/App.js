c0f192f9fca84eeecb70feeb1c7a61402d43b187
Apoorva Shukla <apshuk17@gmail.com>
2019-09-02 10:43:35 +0530

    Created app component with us and dutch flags

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    language: "english"
+  };
+
+  onLanguageChange(language) {
+    this.setState({ language });
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          <h1>Select a language</h1>
+          <i className="flag us" onClick={this.onLanguageChange("english")} />
+          <i className="flag nl" onClick={this.onLanguageChange("dutch")} />
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
